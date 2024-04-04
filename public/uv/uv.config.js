11f2c13a6d9712bfecc420646a98fdd960fa5232
nunera <67439647+nunera@users.noreply.github.com>
2024-04-04 14:31:40 -0500

    womp

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/u/',
-    bare: '/bear/',
+    bare: 'https://bare-server.nunera.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
